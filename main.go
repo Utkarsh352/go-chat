584f916579b930bb72ee8a46e36cc583c7fe36d7
utkarsh352 <utu3528@gmail.com>
2024-08-15 10:36:58 +0530

    Refactor for Vercel serverless functions

--- main.go
+++ main.go
@@ -1,238 +1,244 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"html/template"
-	"log"
-	"net/http"
-	"sync"
-	"time"
+    "bytes"
+    "encoding/json"
+    "html/template"
+    "log"
+    "net/http"
+    "sync"
+    "time"
 
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
+    "github.com/google/uuid"
+    "github.com/gorilla/websocket"
 )
 
+// Define your structures and constants
 type Client struct {
-	id   string
-	hub  *Hub
-	conn *websocket.Conn
-	send chan []byte
+    id   string
+    hub  *Hub
+    conn *websocket.Conn
+    send chan []byte
 }
 
 type Message struct {
-	ClientID string `json:"clientID"`
-	Text     string `json:"text"`
+    ClientID string `json:"clientID"`
+    Text     string `json:"text"`
 }
 
 type Hub struct {
-	sync.RWMutex
-	clients    map[*Client]bool
-	broadcast  chan *Message
-	register   chan *Client
-	unregister chan *Client
-	messages   []*Message
+    sync.RWMutex
+    clients    map[*Client]bool
+    broadcast  chan *Message
+    register   chan *Client
+    unregister chan *Client
+    messages   []*Message
 }
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
 }
 
 const (
-	writeWait      = 10 * time.Second
-	pongWait       = 60 * time.Second
-	pingPeriod     = (pongWait * 9) / 10
-	maxMessageSize = 512
+    writeWait      = 10 * time.Second
+    pongWait       = 60 * time.Second
+    pingPeriod     = (pongWait * 9) / 10
+    maxMessageSize = 512
 )
 
 func NewHub() *Hub {
-	return &Hub{
-		clients:    map[*Client]bool{},
-		broadcast:  make(chan *Message),
-		register:   make(chan *Client),
-		unregister: make(chan *Client),
-	}
+    return &Hub{
+        clients:    map[*Client]bool{},
+        broadcast:  make(chan *Message),
+        register:   make(chan *Client),
+        unregister: make(chan *Client),
+    }
 }
 
 func (h *Hub) run() {
-	for {
-		select {
-		case client := <-h.register:
-			h.Lock()
-			h.clients[client] = true
-			h.Unlock()
+    for {
+        select {
+        case client := <-h.register:
+            h.Lock()
+            h.clients[client] = true
+            h.Unlock()
 
-			log.Printf("client registered %s", client.id)
+            log.Printf("client registered %s", client.id)
 
-			for _, msg := range h.messages {
-				client.send <- getMessageTemplate(msg)
-			}
-		case client := <-h.unregister:
-			h.Lock()
-			if _, ok := h.clients[client]; ok {
-				close(client.send)
-				log.Printf("client unregistered %s", client.id)
-				delete(h.clients, client)
-			}
-			h.Unlock()
-		case msg := <-h.broadcast:
-			h.RLock()
-			h.messages = append(h.messages, msg)
+            for _, msg := range h.messages {
+                client.send <- getMessageTemplate(msg)
+            }
+        case client := <-h.unregister:
+            h.Lock()
+            if _, ok := h.clients[client]; ok {
+                close(client.send)
+                log.Printf("client unregistered %s", client.id)
+                delete(h.clients, client)
+            }
+            h.Unlock()
+        case msg := <-h.broadcast:
+            h.RLock()
+            h.messages = append(h.messages, msg)
 
-			for client := range h.clients {
-				select {
-				case client.send <- getMessageTemplate(msg):
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
-			}
-			h.RUnlock()
-		}
-	}
+            for client := range h.clients {
+                select {
+                case client.send <- getMessageTemplate(msg):
+                default:
+                    close(client.send)
+                    delete(h.clients, client)
+                }
+            }
+            h.RUnlock()
+        }
+    }
 }
 
 func getMessageTemplate(msg *Message) []byte {
-	tmpl, err := template.ParseFiles("templates/message.html")
-	if err != nil {
-		log.Fatalf("template parsing: %s", err)
-	}
+    tmpl, err := template.ParseFiles("templates/message.html")
+    if err != nil {
+        log.Fatalf("template parsing: %s", err)
+    }
 
-	var renderedMessage bytes.Buffer
-	err = tmpl.Execute(&renderedMessage, msg)
-	if err != nil {
-		log.Fatalf("template execution: %s", err)
-	}
+    var renderedMessage bytes.Buffer
+    err = tmpl.Execute(&renderedMessage, msg)
+    if err != nil {
+        log.Fatalf("template execution: %s", err)
+    }
 
-	return renderedMessage.Bytes()
+    return renderedMessage.Bytes()
 }
 
 func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
 
-	id := uuid.New()
+    id := uuid.New()
 
-	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256), id: id.String()}
-	client.hub.register <- client
+    client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256), id: id.String()}
+    client.hub.register <- client
 
-	go client.writePump()
-	go client.readPump()
+    go client.writePump()
+    go client.readPump()
 }
 
 func (c *Client) writePump() {
-	ticker := time.NewTicker(pingPeriod)
+    ticker := time.NewTicker(pingPeriod)
 
-	defer func() {
-		ticker.Stop()
-		c.conn.Close()
-	}()
+    defer func() {
+        ticker.Stop()
+        c.conn.Close()
+    }()
 
-	for {
-		select {
-		case msg, ok := <-c.send:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
+    for {
+        select {
+        case msg, ok := <-c.send:
+            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            if !ok {
+                c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+                return
+            }
 
-			w, err := c.conn.NextWriter(websocket.TextMessage)
-			if err != nil {
-				return
-			}
+            w, err := c.conn.NextWriter(websocket.TextMessage)
+            if err != nil {
+                return
+            }
 
-			w.Write(msg)
+            w.Write(msg)
 
-			n := len(c.send)
-			for i := 0; i < n; i++ {
-				w.Write(msg)
-			}
+            n := len(c.send)
+            for i := 0; i < n; i++ {
+                w.Write(msg)
+            }
 
-			if err := w.Close(); err != nil {
-				return
-			}
+            if err := w.Close(); err != nil {
+                return
+            }
 
-		case <-ticker.C:
-			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
-				return
-			}
-		}
-	}
+        case <-ticker.C:
+            c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+            if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+                return
+            }
+        }
+    }
 }
 
 func (c *Client) readPump() {
-	defer func() {
-		c.hub.unregister <- c
-		c.conn.Close()
-	}()
+    defer func() {
+        c.hub.unregister <- c
+        c.conn.Close()
+    }()
 
-	c.conn.SetReadLimit(maxMessageSize)
-	c.conn.SetReadDeadline(time.Now().Add(pongWait))
-	c.conn.SetPongHandler(func(string) error {
-		c.conn.SetReadDeadline(time.Now().Add(pongWait))
-		return nil
-	})
+    c.conn.SetReadLimit(maxMessageSize)
+    c.conn.SetReadDeadline(time.Now().Add(pongWait))
+    c.conn.SetPongHandler(func(string) error {
+        c.conn.SetReadDeadline(time.Now().Add(pongWait))
+        return nil
+    })
 
-	for {
-		_, text, err := c.conn.ReadMessage()
-		log.Printf("value: %v", string(text))
-		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Printf("error: %v", err)
-			}
-			break
-		}
+    for {
+        _, text, err := c.conn.ReadMessage()
+        log.Printf("value: %v", string(text))
+        if err != nil {
+            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+                log.Printf("error: %v", err)
+            }
+            break
+        }
 
-		msg := &Message{}
+        msg := &Message{}
 
-		reader := bytes.NewReader(text)
-		decoder := json.NewDecoder(reader)
-		err = decoder.Decode(msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-		}
+        reader := bytes.NewReader(text)
+        decoder := json.NewDecoder(reader)
+        err = decoder.Decode(msg)
+        if err != nil {
+            log.Printf("error: %v", err)
+        }
 
-		c.hub.broadcast <- &Message{ClientID: c.id, Text: msg.Text}
-	}
+        c.hub.broadcast <- &Message{ClientID: c.id, Text: msg.Text}
+    }
 }
 
 func serveIndex(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "not found", http.StatusNotFound)
-		return
-	}
+    if r.URL.Path != "/" {
+        http.Error(w, "not found", http.StatusNotFound)
+        return
+    }
 
-	if r.Method != "GET" {
-		http.Error(w, "method not found", http.StatusNotFound)
-		return
-	}
+    if r.Method != "GET" {
+        http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
 
-	http.ServeFile(w, r, "templates/index.html")
+    http.ServeFile(w, r, "templates/index.html")
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		serveIndex(w, r)
-	case "/ws":
-		serveWs(hub, w, r)
-	default:
-		http.Error(w, "not found", http.StatusNotFound)
-	}
+    switch r.URL.Path {
+    case "/":
+        serveIndex(w, r)
+    case "/ws":
+        serveWs(hub, w, r)
+    default:
+        http.Error(w, "not found", http.StatusNotFound)
+    }
+}
+
+// Vercel expects an exported function with the signature: func Handler(w http.ResponseWriter, r *http.Request)
+func Handler(w http.ResponseWriter, r *http.Request) {
+    handler(w, r)
 }
 
 var hub *Hub
 
 func main() {
-	hub = NewHub()
-	go hub.run()
+    hub = NewHub()
+    go hub.run()
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+    http.HandleFunc("/", Handler)
+    log.Fatal(http.ListenAndServe(":3000", nil))
 }
